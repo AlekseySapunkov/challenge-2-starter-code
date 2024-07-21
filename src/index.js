1c78abc979046b5453c0e2cb7403a08bd8202192
Sapunkov Aleksey <vladimir_sapunko@mail.ru>
2024-07-21 20:43:50 +0300

    add Header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
