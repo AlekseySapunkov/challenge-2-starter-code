538b5f047313750885f45e1f93078e02f1766d14
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-07-22 17:53:53 +0300

    add component HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import sushi from "../../assets/M1.Boominfo.ORG - 003 sushi.jpg"
 import styles from './Header.module.css'
-console.log(styles)
+import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
     return <React.Fragment>
         <header className={ styles.header }>
-            <h1>Япона кух</h1>
-            <button>Корзина</button>
+            <h1>Япона кухня</h1>
+            <HeaderCartButton/>
         </header>
         <div className={styles['main-image']}>
             <img src={sushi} alt="Логотип"/>
