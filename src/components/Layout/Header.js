b8ee46d2a117b87af6069305b7d0cf9d696dd4c9
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-07-22 17:33:27 +0300

    add styles modules in header.css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,11 @@
-import "./Header.css";
 import React from "react";
 import sushi from "../../assets/M1.Boominfo.ORG - 003 sushi.jpg"
-import styles from "./Header.css"
+import styles from './Header.module.css'
+console.log(styles)
 const Header = (props) => {
     return <React.Fragment>
-        <header className={styles.header}>
-            <h1>Япона кухня</h1>
+        <header className={ styles.header }>
+            <h1>Япона кух</h1>
             <button>Корзина</button>
         </header>
         <div className={styles['main-image']}>
