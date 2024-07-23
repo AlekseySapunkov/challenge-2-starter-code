18ef409ccb2769e5513aeda9787a46fb1b529e9c
Sapunkov Aleksey <vladimir_sapunko@mail.ru>
2024-07-23 19:11:19 +0300

    upgrade component Meals

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -1,10 +1,11 @@
 import styles from "./MealList.module.css";
-import React from "react";
+import DUMMY_MEALS from "./dummy-meals";
 const MealList = (props) => {
-  return <React.Fragment>
-    <div className={styles.}>
-
-    </div>
-  </React.Fragment>;
+  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <section className={styles.meals}>
+      <ul className={styles.ul}>{mealList}</ul>
+    </section>
+  );
 };
 export default MealList;
