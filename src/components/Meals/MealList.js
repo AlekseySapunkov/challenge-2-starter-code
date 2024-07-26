3addeb071115412ab8ed9ce2ab046a05711be852
Sapunkov Aleksey <vladimir_sapunko@mail.ru>
2024-07-26 17:32:48 +0300

    add component MealItem.js

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -1,10 +1,20 @@
 import styles from "./MealList.module.css";
 import DUMMY_MEALS from "./dummy-meals";
+import Card from "../UI/Card";
+import MealItem from "./MealItem";
 const MealList = (props) => {
-  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={styles.meals}>
-      <ul className={styles.ul}>{mealList}</ul>
+      <Card>
+        <ul className={styles.ul}>{mealList}</ul>
+      </Card>
     </section>
   );
 };
