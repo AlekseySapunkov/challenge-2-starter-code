a10413801f9e07a84fa3b987e6ab8724d6069957
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-08-08 13:48:38 +0300

    fix Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,28 @@
 import Modal from "../UI/Modal"
 import styles from "./Cart.module.css"
 import { useContext } from "react"
-import CartContext from "../../store/cart-contex"
+import CartContext from "../../store/cart-context"
 import CartItem from "./CartItem"
 const Cart = (props) => {
     const cartContext = useContext(CartContext)
     const cartAmount = `$${cartContext.totalAmount.toFixed(2)}`
     const hasItems = cartContext.items.length > 0
+    const removeItemHandler = (id)=>{
+
+    }
+    const addItemHandler = (item)=>{
+
+    }
     const cartItems = (
         <ul className={styles['cart-items']}>
             {cartContext.items.map((item)=>{
-                return <CartItem key ={item.id} name={item.name} amount={item.amount} price = {item.price}/>
+                return <CartItem key ={item.id}
+                 name={item.name} 
+                 amount={item.amount} 
+                 price = {item.price}
+                 onAdd={addItemHandler.bind(null, item)}
+                 onRemove ={removeItemHandler.bind(null, item.id)}
+                 />
             })}
         </ul>
     )
