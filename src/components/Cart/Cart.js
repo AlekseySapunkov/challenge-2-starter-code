92e2d2f8d441ecff42e43a8bb33ed68401bd4f16
Sapunkov Aleksey <vladimir_sapunko@mail.ru>
2024-08-14 19:55:55 +0300

    Update CartContextProvider.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,42 +1,49 @@
-import Modal from "../UI/Modal"
-import styles from "./Cart.module.css"
-import { useContext } from "react"
-import CartContext from "../../store/cart-context"
-import CartItem from "./CartItem"
+import Modal from "../UI/Modal";
+import styles from "./Cart.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 const Cart = (props) => {
-    const cartContext = useContext(CartContext)
-    const cartAmount = `$${cartContext.totalAmount.toFixed(2)}`
-    const hasItems = cartContext.items.length > 0
-    const removeItemHandler = (id)=>{
+  const cartContext = useContext(CartContext);
+  const cartAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const hasItems = cartContext.items.length > 0;
+  const removeItemHandler = (id) => {
+    cartContext.removeItem(id);
+  };
+  const addItemHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartContext.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={addItemHandler.bind(null, item)}
+            onRemove={removeItemHandler.bind(null, item.id)}
+          />
+        );
+      })}
+    </ul>
+  );
 
-    }
-    const addItemHandler = (item)=>{
-
-    }
-    const cartItems = (
-        <ul className={styles['cart-items']}>
-            {cartContext.items.map((item)=>{
-                return <CartItem key ={item.id}
-                 name={item.name} 
-                 amount={item.amount} 
-                 price = {item.price}
-                 onAdd={addItemHandler.bind(null, item)}
-                 onRemove ={removeItemHandler.bind(null, item.id)}
-                 />
-            })}
-        </ul>
-    )
-
-    return <Modal onHideCart ={props.onHideCart}>
-        {cartItems}
-        <div className={styles.total}>
-            <span>Итого</span>
-            <span> {cartAmount}</span>
-        </div>
-        <div className={styles.actions}>
-            <button className={styles['button--alt']} onClick={props.onHideCart}>Закрыть</button>
-            {hasItems && <button className={styles.button}>Заказать</button>}
-        </div>
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span> {cartAmount}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]} onClick={props.onHideCart}>
+          Закрыть
+        </button>
+        {hasItems && <button className={styles.button}>Заказать</button>}
+      </div>
     </Modal>
-}
-export default Cart
+  );
+};
+export default Cart;
