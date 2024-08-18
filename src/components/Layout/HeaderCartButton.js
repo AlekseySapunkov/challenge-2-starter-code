c5a0d51d03374958cfbc051bc1283755990509a5
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-08-18 12:17:00 +0300

    add  animation HeaderCartButton.js

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,26 @@
 import CartContext from "../../store/cart-context"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import styles from "./HeaderCartButton.module.css"
 import CartIcon from "../Cart/CartIcon"
 const HeaderCartButton = (props) =>{
+    const[isButtonAnimated, setIsButtonAnimated]= useState(false)
    const cartContext = useContext(CartContext)
     const cartItemsAmount = cartContext.items.reduce((currentValue, item)=>{
         return currentValue + item.amount
     },0)
-    return <button className={styles.button} onClick={props.onClick}>
+
+    const buttonClasses = `${styles.button} ${isButtonAnimated ? styles.bump : ''}`
+    useEffect(()=>{
+        if(cartContext.items.length === 0){
+            return
+        }
+        setIsButtonAnimated(true)
+       const timer =  setTimeout(() => {setIsButtonAnimated(false)}, 300)
+       return ()=>{
+        clearTimeout(timer)
+       }
+    }, [cartContext.items])
+    return <button className={buttonClasses} onClick={props.onClick}>
         <span className={styles.icon}><CartIcon/></span>
         <span>Корзина</span>
         <span className={styles.badge}>{cartItemsAmount}</span>
